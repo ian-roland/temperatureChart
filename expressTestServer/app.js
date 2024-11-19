a74feb8523b505de38d44d337f05650e50d31849
Ian Roland <ianrolandnascimento@gmail.com>
2024-11-19 16:23:21 -0300

    created: the initial sources to develop the frontend on the chart temperature tracker on the raspberry pi cm4, using vite, react and chartjs

--- expressTestServer/app.js
+++ expressTestServer/app.js
@@ -2,11 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+
 app.get('/', (req, res) => {
-   
-   
     res.send("Hello World!");
 })
+
+app.get('/about', (req, res) => {
+    res.send("About");
+})
+
+app.get('/blog', (req, res) => {
+    res.send("Blog");
+})
+
+app.get('/contact', (req, res) => {
+    res.send("Contact");
+})
+
 
 
 
