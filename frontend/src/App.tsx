877240c6b486b26b1c2e89216bbec61bd08c5b1a
Ian Roland <ianrolandnascimento@gmail.com>
2024-12-05 16:57:36 -0300

    added: a simple button for turning on and off the fan

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,28 @@
 import { ChartComponent } from "./components/chartComponent";
+import { ButtonOutline } from "./components/Button";
 import piIcon from "./assets/piIcon.svg";
 import cddIcon from "./assets/cddLogo.png";
 
 function App() {
   return (
     <>
-      <div className="justify-center flex">
-        <div className="size-[1300px] mb-0 overflow-hidden max-h-screen pt-6 px-4">
-          <ChartComponent />
+    
+      <div className="justify-center flex w-full min-h-screen pt-6 px-2 gap-2">
+        <div className="size-[1300px] mb-0 overflow-hidden max-h-screen">
+          <ChartComponent/>
         </div>
-
-        <div id="logos" className="w-36 pt-4">
-          <img src={piIcon} alt="RaspberryPiLogo" />
-          <img src={cddIcon} alt="ColeDDLogo" />
+       
+        <div className="flex flex-col size-32 space-y-4">
+          
+          <img src={piIcon} className="w-full" alt="RaspberryPiLogo" />
+          <img src={cddIcon} className="w-full" alt="ColeDDLogo" />
+          <ButtonOutline></ButtonOutline>
         </div>
+  
+        
       </div>
+
+      
     </>
   );
 }
