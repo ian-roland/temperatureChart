7cd4f291444234d5b480ff4c2ee161d6edde3d5b
Ian Roland <ianrolandnascimento@gmail.com>
2024-12-10 12:36:13 -0300

    feat: added code to the ButtonOutline.tsx for toggling the fan

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,5 +1,20 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export function ButtonOutline() {
-  return <Button variant="outline">Turn the Fan</Button>
+  const [fanState, setFanState] = useState(false);
+
+  const toogleFan = () => {
+    setFanState(!fanState);
+    fetch("/toggle_manual_fan", { method: "POST" })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error toggling fan:", error));
+  };
+
+  return (
+    <Button onClick={toogleFan} variant="outline">
+      {fanState ? 'Turn Off Fan' : 'Turn On Fan'}
+    </Button>
+  );
 }
