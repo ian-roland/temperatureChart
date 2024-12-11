3fd90f5a4e72bd5c87ed5f2b0c5d1b967be6d1ab
Ian Roland <ianrolandnascimento@gmail.com>
2024-12-11 17:02:09 -0500

    fix: removed the control_temperature from the temperature_and_datetime to make it work again (temporary)

--- backend/tempScript.py
+++ backend/tempScript.py
@@ -29,7 +29,6 @@ def get_temperature_and_datetime():
             result = subprocess.run(['vcgencmd', 'measure_temp'], stdout=subprocess.PIPE, check=True)
             output = result.stdout.decode('utf-8').strip()
             temperature = float(output.split('=')[1].strip("'C"))
-            control_fan(temperature)
         except subprocess.CalledProcessError:
             temperature = None  # Indicate temperature unavailable
 
